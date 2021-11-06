e9ce41b1d938e28d6f9aaa98955add60c7b99ec8
John Goodman <john.goodman@gmail.com>
2021-11-06 13:09:32 -0700

    created pip package at https://pypi.org/project/randvars/

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md') as readme_file:
+    README = readme_file.read()
 
-setuptools.setup(
-     name='randvariates',
-     version='0.1',
-     scripts=['RV.py'],
-     author="John Goodman",
-     author_email="john.goodman@gmail.com",
-     description="A library of random variate generation routines",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/jgoodie/randomvariates",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='randvars',
+    version='0.0.1',
+    description='A library of random variate generation routines',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='John Goodman',
+    author_email='john.goodman@gmail.com',
+    keywords=['Random', 'Random Generator', 'Random Variates', 'Random Variables'],
+    url='https://github.com/jgoodie/randomvariates',
+    download_url='https://pypi.org/project/randvars/'
+)
+
+install_requires = [
+    'numpy'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires, include_package_data=True)
